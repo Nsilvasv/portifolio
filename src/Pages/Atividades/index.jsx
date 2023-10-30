cf7bbff5ce4bb566b473944343d58102db359492
Nathan <nathansilva406@gmail.com>
2023-10-30 15:48:29 -0300

    consumindo api github

--- src/Pages/Atividades/index.jsx
+++ src/Pages/Atividades/index.jsx
@@ -1,7 +1,44 @@
+import { useState, useEffect  } from "react"
+import "./index.css"
+import Card from "../../Components/Card"
+
 const Atividades = () => {
+
+    const [repositorio, setRepositorio] = useState([])
+
+    useEffect( () => {
+        const busca = async () => {
+
+            const response = await fetch("https://api.github.com/users/Nsilvasv/repos")
+            const data = await response.json()
+            setRepositorio(data)
+        }
+        busca()
+    }, [])
+
     return(
-        <div>
-            <h1>Em criação...</h1>
+        <div className="container">
+
+            {
+                repositorio.length > 0 ? (
+                    
+                    <div className="atividades">
+                        {repositorio.map( (repo) => (
+                            <Card key={repo.id} 
+                                name={repo.name} 
+                                description={repo.description} 
+                                html_url={repo.html_url} 
+                             />
+                        ))}
+
+                    </div>
+                ) : (
+
+                <p>Carregando repositórios...</p>
+                
+                )
+            }
+
         </div>
     )
 }
