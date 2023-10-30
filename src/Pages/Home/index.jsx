66050350318371258578863a022452f749973ac7
Nathan <nathansilva406@gmail.com>
2023-10-30 15:50:27 -0300

    home

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,52 @@
-import Banner from "../../Components/Banner"
+import "./index.css"
+import { Link } from 'react-router-dom'
+import { BsInstagram, BsGithub, BsLinkedin } from 'react-icons/bs';
+import { Typewriter } from 'react-simple-typewriter'
+import ft from "../../img/ft.avif"
+import Button from "../../Components/Button";
+
 
 const Home = () => {
+
     return(
-        <>
-            <Banner/>
-        </>
+        <div className="home">
+
+            <div className="apresentacao">
+
+                <h1 className="titulo">Olá, Eu sou Nathan</h1>
+
+                <span className="subtitulo">
+                        {/* Style will be inherited from the parent element */}
+                        <Typewriter
+                            words={[' Desenvolvedor web', ' Bem-vindo!']}
+                            loop={Infinity}
+                            cursor
+                            cursorStyle='|'
+                            typeSpeed={95}
+                            deleteSpeed={60}
+                            delaySpeed={1000}
+                      
+                        />
+                </span>
+
+            
+                <div>
+                    <ul className='lista-icons'>
+
+                        <li className='icons'> <Link to="https://github.com/Nsilvasv"><BsGithub/></Link> </li>
+                        <li className='icons'> <Link to="https://www.instagram.com/nathansv_/"> <BsInstagram/></Link></li>   
+                        <li className='icons'> <Link to="https://www.linkedin.com/in/nathan-silva-sv"><BsLinkedin/></Link> </li>
+                    </ul>
+                </div>
+                
+            </div>
+
+            <div className="imagens">
+                <img className="minhaFoto" src={ft} />
+            </div>
+
+         
+        </div>
     )
 }
 
